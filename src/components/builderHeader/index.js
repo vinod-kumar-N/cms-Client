c30aca244847d02a43468c873f9eedc13573c75d
Lakshminarayana Andra <lakshminarayana.andra@ascenaretail.com>
2020-05-07 22:50:53 +0530

    color picker

--- src/components/builderHeader/index.js
+++ src/components/builderHeader/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./index.scss";
 import API from "../../api";
+import Sketch from "../builderHeader/colorPicker";
+import reactCSS from "reactcss";
+import { SketchPicker } from "react-color";
 class BuilderHeader extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       file: "logo-client.png",
+      displayColorPicker: false,
+      color: {
+        r: "52",
+        g: "73",
+        b: "94",
+        a: "1",
+      },
     };
   }
   handleChange = (event) => {
@@ -36,9 +46,50 @@ class BuilderHeader extends React.Component {
   callFileUpload = () => {
     this.inputElement.click();
   };
+  handleClick = () => {
+    this.setState({ displayColorPicker: !this.state.displayColorPicker });
+  };
+
+  handleClose = () => {
+    this.setState({ displayColorPicker: false });
+  };
+
+  handleChange = (color) => {
+    this.setState({ color: color.rgb });
+  };
   render() {
+    const styles = reactCSS({
+      default: {
+        color: {
+          width: "1000px",
+          height: "30px",
+          borderRadius: "2px",
+          background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
+        },
+        swatch: {
+          padding: "5px",
+          background: "#fff",
+          borderRadius: "1px",
+          boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
+          display: "inline-block",
+          cursor: "pointer",
+        },
+        popover: {
+          position: "absolute",
+          zIndex: "2",
+        },
+        cover: {
+          position: "fixed",
+          top: "0px",
+          right: "0px",
+          bottom: "0px",
+          left: "0px",
+        },
+      },
+    });
+
     return (
-      <header>
+      <header className="header-section">
         <div class="leftSection">
           <div className="logo-container" onClick={this.callFileUpload}>
             <img src={this.state.file} alt="logo"></img>
@@ -50,7 +101,22 @@ class BuilderHeader extends React.Component {
             />
           </div>
         </div>
-        <div class="rightSection"></div>
+        <div class="rightSection">
+          <div>
+            <div style={styles.swatch} onClick={this.handleClick}>
+              <div style={styles.color} />
+            </div>
+            {this.state.displayColorPicker ? (
+              <div style={styles.popover}>
+                <div style={styles.cover} onClick={this.handleClose} />
+                <SketchPicker
+                  color={this.state.color}
+                  onChange={this.handleChange}
+                />
+              </div>
+            ) : null}
+          </div>
+        </div>
       </header>
     );
   }
--- src/components/pageBuilder/index.js
+++ src/components/pageBuilder/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import BuilderHeader from "../builderHeader";
+import Sketch from "../builderHeader/colorPicker";
 class PageBuilder extends React.Component {
   render() {
     return (
       <div className="page-builder">
         <BuilderHeader />
+        <Sketch />
       </div>
     );
   }
